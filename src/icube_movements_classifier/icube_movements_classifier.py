d4d545c407c72cbb358f81c9d78517697278c3a1
Sophia.Capellini <sophia.capellini@iit.it>
2023-11-02 14:05:31 +0100

    new part for the matb_experiment

--- src/icube_movements_classifier/icube_movements_classifier.py
+++ src/icube_movements_classifier/icube_movements_classifier.py
@@ -209,11 +209,11 @@ class MovementsDetector(BaseHandler):
             if self.__icube_posed(touches):
                 self.icube_state = MovementState.POSED
                 self.on_pose()
-            """""
+
             else:
                 self.icube_state = MovementState.GRABBED
                 self.on_grab()
-            """
+            
 
         np_acc = np.array(accelerometer)
 
