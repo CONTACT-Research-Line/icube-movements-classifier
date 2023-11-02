51c7f6ef3c33e5dc391c66d330fc3c6352dbcf29
Sophia.Capellini <sophia.capellini@iit.it>
2023-11-02 16:22:22 +0100

    new part for the matb_experiment

--- src/icube_movements_classifier/icube_movements_classifier.py
+++ src/icube_movements_classifier/icube_movements_classifier.py
@@ -280,7 +280,7 @@ class MovementsDetector(BaseHandler):
 
         "handling quaternions"
         "convertion from quaternions to angles"
-        delta,somma_rotazioni = self.compute_angles()
+        delta,somma_rotazioni = self.compute_angles(1)
 
         "handling accelerometer"
         if accelerometer is None or accelerometer == []:
