f7019eb693936e6b34f03d0a030189d526161095
Sophia.Capellini <sophia.capellini@iit.it>
2023-11-24 14:33:24 +0100

    new part for the matb_experiment

--- src/icube_movements_classifier/icube_movements_classifier.py
+++ src/icube_movements_classifier/icube_movements_classifier.py
@@ -624,7 +624,7 @@ class MovementsDetector(BaseHandler):
                 self.on_pose()
 
         if self.icube_state == MovementState.POSED:
-            if self.__icube_corner_face(touches, face_id):
+            if self.__icube_corner_face(touches, face_id=1):
                 self.icube_state = MovementState.TOUCH_FIRSTFACE_UPLEFT
                 self.on_touch_firstface_upleft()
 
