9894f287bea0b4a15c6a592b457d002b80458187
Dario <dario.pasquali@iit.it>
2024-05-29 13:51:03 +0200

    Factory passing parameters

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 
 from icube import ICubeInterface
 from icube import ICubeVersion
+from icube_movements_classifier import ButtonEvents
 
 from icube_movements_classifier.IMCFactory import *
 from icube_movements_classifier.imc_matb import MatbMovementEvents
@@ -28,33 +29,16 @@ def main():
     device = ICubeInterface(version=ICubeVersion.V3, enable_gui=("--gui" in sys.argv))
     device.init(name="cube_test", serial_port="/dev/ttyUSB0")
 
-    gp_classifier = factory.get_movement_classifier("base")()
+    gp_classifier = factory.get_movement_classifier(IMC.GRAB_POSE, grab_tolerance=1.0)
     gp_classifier.set_callback(GrabPoseEvents.GRAB, lambda: print("GRAB"))
     gp_classifier.set_callback(GrabPoseEvents.POSE, lambda: print("POSE"))
     device.bind_callback(gp_classifier.handle)
 
-    swipes_classifier = factory.get_movement_classifier("swipes")(detect_on_face=0)
-    swipes_classifier.set_callback(SwipeEvents.COVER, lambda x: print(f"COVER {x}"))
-    swipes_classifier.set_callback(SwipeEvents.FORWARD, lambda x: print(f"FORWARD {x}"))
-    swipes_classifier.set_callback(SwipeEvents.BACKWARD, lambda x: print(f"BACKWARD {x}"))
-    device.bind_callback(swipes_classifier.handle)
-
-    matb = factory.get_movement_classifier("matb")()
-    matb.set_callback(MatbMovementEvents.TOUCH_RIGHTFACE, lambda: print("TOUCH_RIGHTFACE"))
-    matb.set_callback(MatbMovementEvents.TOUCH_LEFTFACE, lambda: print("TOUCH_LEFTFACE"))
-    matb.set_callback(MatbMovementEvents.TOUCH_FRONTFACE, lambda: print("TOUCH_FRONTFACE"))
-    matb.set_callback(MatbMovementEvents.TOUCH_BACKFACE, lambda: print("TOUCH_BACKFACE"))
-    matb.set_callback(MatbMovementEvents.TOUCH_TOPFACE_TOPLEFT, lambda: print("TOUCH_TOPFACE_TOPLEFT"))
-    matb.set_callback(MatbMovementEvents.TOUCH_TOPFACE_TOPRIGHT, lambda: print("TOUCH_TOPFACE_TOPRIGHT"))
-    matb.set_callback(MatbMovementEvents.TOUCH_TOPFACE_BOTTOMLEFT, lambda: print("TOUCH_TOPFACE_BOTTOMLEFT"))
-    matb.set_callback(MatbMovementEvents.TOUCH_TOPFACE_BOTTOMRIGHT, lambda: print("TOUCH_TOPFACE_BOTTOMRIGHT"))
-    matb.set_callback(MatbMovementEvents.ROTATE_RIGHT, lambda: print("ROTATE_RIGHT"))
-    matb.set_callback(MatbMovementEvents.ROTATE_LEFT, lambda: print("ROTATE_LEFT"))
-    matb.set_callback(MatbMovementEvents.ROTATE_FORWARD, lambda: print("ROTATE_FORWARD"))
-    matb.set_callback(MatbMovementEvents.ROTATE_BACKWARD, lambda: print("ROTATE_BACKWARD"))
-    matb.set_callback(MatbMovementEvents.TURNED_CLOCKWISE, lambda: print("TURNED_CLOCKWISE"))
-    matb.set_callback(MatbMovementEvents.TURNED_ANTICLOCKWISE, lambda: print("TURNED_ANTICLOCKWISE"))
-    device.bind_callback(matb.handle)
+    button_classifier = factory.get_movement_classifier(IMC.BUTTON_PRESS,
+                                                        detect_on_face=None, release_after=2.0, min_cover=10)
+    button_classifier.set_callback(ButtonEvents.PRESS, lambda x: print(f"PRESS {x}"))
+    button_classifier.set_callback(ButtonEvents.RELEASE, lambda x: print(f"RELEASE {x}"))
+    device.bind_callback(button_classifier.handle)
 
     device.start_streaming()
     time.sleep(100000)  # seconds
--- src/icube_movements_classifier/IMCFactory.py
+++ src/icube_movements_classifier/IMCFactory.py
@@ -1,3 +1,5 @@
+import enum
+
 from icube_movements_classifier import MatbMovementDetector
 from icube_movements_classifier import GrabPoseDetector
 from icube_movements_classifier import SwipeDetector
@@ -5,19 +7,27 @@ from icube_movements_classifier import DoubleFullTouchDetector
 from icube_movements_classifier import ButtonPressDetector
 
 
+class IMC(enum.Enum):
+    GRAB_POSE = 0,
+    DOUBLE_TOUCH = 1,
+    SWIPE = 2,
+    MATB = 3,
+    BUTTON_PRESS = 4
+
+
 class IMCFactory:
     def __init__(self):
         self.available_classifiers = {
-            "base": GrabPoseDetector,
-            "double_touch": DoubleFullTouchDetector,
-            "swipes": SwipeDetector,
-            "matb": MatbMovementDetector,
-            "button_press": ButtonPressDetector
+            IMC.GRAB_POSE: GrabPoseDetector,
+            IMC.DOUBLE_TOUCH: DoubleFullTouchDetector,
+            IMC.SWIPE: SwipeDetector,
+            IMC.MATB: MatbMovementDetector,
+            IMC.BUTTON_PRESS: ButtonPressDetector
         }
 
-    def get_movement_classifier(self, classifier_type):
-        if classifier_type in self.available_classifiers:
-            return self.available_classifiers[classifier_type]
+    def get_movement_classifier(self, imc, **kwargs):
+        if imc in self.available_classifiers:
+            return self.available_classifiers[imc](**kwargs)
 
         raise ValueError("Unavailable classifier")
 
